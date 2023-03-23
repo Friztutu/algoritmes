3ef85f446e644267ccc901df90cc09c01514ecdb
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-03-23 21:30:16 +0300

    (ADD) Add tests for binary search method

--- search_algoritmes/tests.py
+++ search_algoritmes/tests.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
-from Searches import Searches
+from search_algoritmes.Searches import Searches
+from search_algoritmes.exceptions import *
 
 
 class SearchesTests(TestCase):
 
     def binary_search_tests(self):
-        self.assertEqual(Searches.binary_search([1, 2, 3, 4, 5, 6, 7, 8]), 2)
+        self.assertEqual(Searches.binary_search([1, 2, 3, 4, 5, 6, 7, 8], 3), 2)
+        self.assertEqual(Searches.binary_search([1, 2, 3, 4, 5, 6, 7, 8], 1), 2)
+        self.assertEqual(Searches.binary_search([1, 2, 3, 4, 5, 6, 7, 8], 8), 2)
+        self.assertRaises(SearchesIncorrectType, Searches.binary_search(), {1, 2, 3, 4, 5, 6, 7, 8}, 8)
+        self.assertRaises(SearchesIncorrectType, Searches.binary_search(), [1, '2', 3, 4, 5, 6, 7, 8], 8)
+        self.assertRaises(SearchesUnsortedList, Searches.binary_search(), [1, 2, 3, 5, 4, 6, 7, 8], 8)
 
